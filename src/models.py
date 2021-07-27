f4203f487404e1bd3518fd9c3b57e2cb4d2acb19
legg2710 <luisemilio2710@gmail.com>
2021-07-27 23:59:49 +0000

    family tree 50%

--- src/models.py
+++ src/models.py
@@ -1,19 +1,69 @@
 from flask_sqlalchemy import SQLAlchemy
+from random import randint
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class FamilyTree:
+    def __init__(self, last):
+        self.last = last
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+        self.members = {
+            'id': 1,
+            'first': 'Abuelo',
+            'last': 'self.last',
+            'age': '32',
+            'parent': 'none',
+            'child': '2, 3'    
+        },
+         self.members = {
+            'id': 2,
+            'first': 'Abuelo',
+            'last': 'self.last',
+            'age': '32',
+            'parent': 'none',
+            'child': '2, 3'    
+        },
+         self.members = {
+            'id': 3,
+            'first': 'Abuelo',
+            'last': 'self.last',
+            'age': '32',
+            'parent': 'none',
+            'child': '2, 3'    
+        },
+         self.members = {
+            'id': 4,
+            'first': 'Abuelo',
+            'last': 'self.last',
+            'age': '32',
+            'parent': 'none',
+            'child': '2, 3'    
+        },
+         self.members = {
+            'id': 5,
+            'first': 'Abuelo',
+            'last': 'self.last',
+            'age': '32',
+            'parent': 'none',
+            'child': '2, 3'    
+        },
+         self.members = {
+            'id': 6,
+            'first': 'Abuelo',
+            'last': 'self.last',
+            'age': '32',
+            'parent': 'none',
+            'child': '2, 3'    
+        },
+         self.members = {
+            'id': 7,
+            'first': 'Abuelo',
+            'last': 'self.last',
+            'age': '32',
+            'parent': 'none',
+            'child': '2, 3'    
+        },
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        # def generateId (self):
+        #     return randint (0, 9999999999999)
+        #     def addMember(self,{data})
