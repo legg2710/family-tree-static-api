99e3f64f8082571c5012b464a93519b77de493c4
legg2710 <luisemilio2710@gmail.com>
2021-07-28 01:02:37 +0000

    data update

--- src/main.py
+++ src/main.py
@@ -7,18 +7,19 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
+
+from models import FamilyTree
 #from models import Person
+family = FamilyTree('Gonzalez')
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
-db.init_app(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# MIGRATE = Migrate(app, db)
+# db.init_app(app)
 CORS(app)
-setup_admin(app)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,14 +31,16 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/members', methods=['GET'])
+def handle_all():
+    return jsonify(family.members), 200
 
-    return jsonify(response_body), 200
+@app.route('/members/<int:member_id>', methods=['GET'])
+def handle_member(member_id=None):
+    llama = family.memberId(member_id)
+    return jsonify(llama), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,69 +1,75 @@
 from flask_sqlalchemy import SQLAlchemy
-from random import randint
+from random import randint #libreria que entrega un numero aleatorio
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 class FamilyTree:
     def __init__(self, last):
         self.last = last
 
-        self.members = {
-            'id': 1,
-            'first': 'Abuelo',
-            'last': 'self.last',
-            'age': '32',
-            'parent': 'none',
-            'child': '2, 3'    
+        self.members = [ { 
+            'id' : 1,
+            'first' : 'Abuelo',
+            'last' : self.last,
+            'age' : 32,
+            'parent' : None,
+            'child' : [2, 3]
+        } ,
+        {
+            'id' : 2,
+            'first' : 'Tio',
+            'last' : self.last,
+            'age' : 32,
+            'parent' : 1,
+            'child' : None
         },
-         self.members = {
-            'id': 2,
-            'first': 'Abuelo',
-            'last': 'self.last',
-            'age': '32',
-            'parent': 'none',
-            'child': '2, 3'    
+        {
+            'id' : 3,
+            'first' : 'Padre',
+            'last' : self.last,
+            'age' : 32,
+            'parent' : 1,
+            'child' : [4]
         },
-         self.members = {
-            'id': 3,
-            'first': 'Abuelo',
-            'last': 'self.last',
-            'age': '32',
-            'parent': 'none',
-            'child': '2, 3'    
+        {
+            'id' : 4,
+            'first' : 'Luis',
+            'last' : self.last,
+            'age' : 32,
+            'parent' : 3,
+            'child' : None
         },
-         self.members = {
-            'id': 4,
-            'first': 'Abuelo',
-            'last': 'self.last',
-            'age': '32',
-            'parent': 'none',
-            'child': '2, 3'    
+        {
+            'id' : 5,
+            'first' : 'Luis Hermano',
+            'last' : self.last,
+            'age' : 32,
+            'parent' : 3,
+            'child' : None
         },
-         self.members = {
-            'id': 5,
-            'first': 'Abuelo',
-            'last': 'self.last',
-            'age': '32',
-            'parent': 'none',
-            'child': '2, 3'    
+        {
+            'id' : 6,
+            'first' : 'Tio2',
+            'last' : self.last,
+            'age' : 32,
+            'parent' : 1,
+            'child' : [7]
         },
-         self.members = {
-            'id': 6,
-            'first': 'Abuelo',
-            'last': 'self.last',
-            'age': '32',
-            'parent': 'none',
-            'child': '2, 3'    
-        },
-         self.members = {
-            'id': 7,
-            'first': 'Abuelo',
-            'last': 'self.last',
-            'age': '32',
-            'parent': 'none',
-            'child': '2, 3'    
-        },
+        {
+            'id' : 7,
+            'first' : 'Primo',
+            'last' : self.last,
+            'age' : 32,
+            'parent' : 6,
+            'child' : None
+        }
+        ]
+    
+    def memberId(self, id):
+        for miembro in self.members:
+            if miembro['id'] == int(id):
+                return miembro
+        return None
 
-        # def generateId (self):
-        #     return randint (0, 9999999999999)
-        #     def addMember(self,{data})
+    # def generateId(self):
+    #     return randint(0, 99999999999999)
